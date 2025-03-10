f2073a3ce285b2388d752186cefaab4bf68f1de5
JoseGTD <josgardel8@alum.us.es>
2025-03-10 11:27:17 +0100

    fix service money poroperty csv error

--- src/main/java/acme/entities/service/Service.java
+++ src/main/java/acme/entities/service/Service.java
@@ -5,12 +5,13 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidUrl;
 import acme.constraints.ValidDwellTime;
-import acme.constraints.ValidMoney;
 import acme.constraints.ValidPromotionCode;
 import acme.constraints.ValidShortText;
 import lombok.Getter;
@@ -50,7 +51,7 @@ public class Service extends AbstractEntity {
 	@Optional
 	@Automapped
 	@ValidMoney
-	private Double				money;
+	private Money				money;
 
 	// Derived attributes -----------------------------------------------------
 

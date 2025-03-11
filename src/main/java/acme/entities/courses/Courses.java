586cbb2b6a64905973412fa25efc2a8af8a1934d
HHV4884-ETSII <luccamdie@alum.us.es>
2025-03-11 21:05:27 +0100

    Automapped

--- src/main/java/acme/entities/courses/Courses.java
+++ src/main/java/acme/entities/courses/Courses.java
@@ -11,6 +11,7 @@ import javax.validation.constraints.Min;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.constraints.ValidLongText;
 import acme.constraints.ValidShortText;
@@ -29,50 +30,61 @@ public class Courses extends AbstractEntity {
 
 	@Mandatory
 	@ValidShortText
+	@Automapped
 	String						name;
 
 	@Mandatory
 	@ValidLongText
+	@Automapped
 	String						description;
 
 	@Mandatory
 	@ValidShortText
+	@Automapped
 	String						courseCategory;
 
 	@Mandatory
 	@Valid
+	@Automapped
 	Integer						courseDuration;
 
 	@Mandatory
 	@ValidShortText
+	@Automapped
 	String						courseInstructor;
 
 	@Mandatory
 	@Valid
+	@Automapped
 	Money						coursePrice;
 
 	@Mandatory
 	@ValidShortText
+	@Automapped
 	String						language;
 
 	@Mandatory
 	@Future
 	@Valid
+	@Automapped
 	Date						courseStartDate;
 
 	@Mandatory
 	@Valid
 	@Future
+	@Automapped
 	Date						courseEndDate;
 
 	@Mandatory
 	@Valid
+	@Automapped
 	Boolean						certification;
 
 	@Mandatory
 	@Valid
 	@Min(1)
 	@Max(500)
+	@Automapped
 	Integer						numberInscriptions;
 
 	// Derived attributes -----------------------------------------------------

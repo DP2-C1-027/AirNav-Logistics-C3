01af1057289713b0b49f3c9b67bf52941ed0d77c
norapfr <norpennfri@alum.us.es>
2025-03-19 19:26:25 +0100

    airline (falta update & create)

---
+++ src/main/java/acme/features/administrator/airline/AdministratorAirlineListService.java
@@ -0,0 +1,48 @@
+
+package acme.features.administrator.airline;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.principals.Administrator;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.airline.Airline;
+
+@GuiService
+public class AdministratorAirlineListService extends AbstractGuiService<Administrator, Airline> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AdministratorAirlineRepository repository;
+
+	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<Airline> anouncements;
+
+		anouncements = this.repository.findAllAirline();
+
+		super.getBuffer().addData(anouncements);
+	}
+
+	@Override
+	public void unbind(final Airline airline) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(airline, "name", "code", "type");
+
+		super.getResponse().addData(dataset);
+
+	}
+}
--- src/main/java/acme/features/customers/booking/CustomersBookingShowService.java
+++ src/main/java/acme/features/customers/booking/CustomersBookingShowService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import acme.client.components.datatypes.Money;
 import acme.client.components.models.Dataset;
-import acme.client.components.views.SelectChoices;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
 import acme.entities.booking.Booking;
@@ -52,15 +51,13 @@ public class CustomersBookingShowService extends AbstractGuiService<Customers, B
 	public void unbind(final Booking booking) {
 		Dataset dataset;
 		Collection<Passenger> passenger;
-		SelectChoices choices;
+
 		Money price = booking.getPrice();
 		passenger = this.repository.findPassengersByBookingId(booking.getId());
 
 		dataset = super.unbindObject(booking, "locatorCode", "purchaseMoment", "travelClass", "lastNibble");
 		dataset.put("price", price);
-		//price no se ve nada y no se como hacer la lista de passenger
-		for (Passenger p : passenger)
-			dataset.put("passenger", p.getFullName());
+
 		super.getResponse().addData(dataset);
 	}
 }

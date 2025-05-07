cdf293a872241ebfa2da92bc2003cbf424bfaadc
norapfr <norpennfri@alum.us.es>
2025-05-07 13:07:01 +0200

    indices grupales

--- src/main/java/acme/entities/airport/Airport.java
+++ src/main/java/acme/entities/airport/Airport.java
@@ -5,6 +5,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.Index;
+import javax.persistence.Table;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
@@ -22,6 +24,10 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+
+@Table(indexes = {
+	@Index(columnList = "codigo")
+})
 public class Airport extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
